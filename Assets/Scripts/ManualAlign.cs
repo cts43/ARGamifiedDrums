0e9bc8c48cce692632101d40178ec7255ef151c5
Chris <seadonchristopher@gmail.com>
2025-06-13 18:11:40 +0100

    Add basic alignment of physical objects for aligning virtual drums with real ones

---
+++ Assets/Scripts/ManualAlign.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ManualAlign : MonoBehaviour
+{
+
+    private GameObject RightHandAnchor;
+    private GameObject MoveableScene;
+    private bool align = false;
+
+    private void Start()
+    {
+        RightHandAnchor = GameObject.FindGameObjectWithTag("RightHandAnchor");
+        MoveableScene = GameObject.FindGameObjectWithTag("Moveable Scene");
+    }
+
+    private void Update()
+    {
+
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            align = !align; //Toggle alignment when 'A' button pressed
+        }
+
+        if (align)
+        {
+            var newXPos = RightHandAnchor.transform.position.x;
+            var newYPos = MoveableScene.transform.position.y; //existing position
+            var newZPos = RightHandAnchor.transform.position.z;
+
+            var newXRot = MoveableScene.transform.eulerAngles.x;
+            var newYRot = RightHandAnchor.transform.eulerAngles.y; //right hand rotation
+            var newZRot = MoveableScene.transform.eulerAngles.z;
+
+            MoveableScene.transform.position = (new Vector3(newXPos, newYPos, newZPos));
+            MoveableScene.transform.rotation = Quaternion.Euler(new Vector3(newXPos, newYRot, newZPos));
+        }  
+    }
+}
