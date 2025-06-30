f8d6a3dbe8a599b9b977895460799f6e1d9ce889
Chris <seadonchristopher@gmail.com>
2025-06-30 15:34:06 +0100

    Add font files

---
+++ Assets/Scripts/ScoreIndicator.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ScoreIndicator : MonoBehaviour
+{
+
+    public int clearTime = 100;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    public IEnumerator ReplaceLabel(string stringToReplace)
+    {
+        var label = GetComponentInChildren<TextMeshProUGUI>();
+        label.text = stringToReplace;
+        for (int i = 0; i < clearTime; i++)
+        {
+            yield return null; //wait clearTime frames
+        }
+        label.text = "";
+    }
+
+    public IEnumerator AddToLabel(string stringToAdd)
+    {
+        var label = GetComponentInChildren<TextMeshProUGUI>();
+        label.text = label.text + "\n" + stringToAdd;
+        for (int i = 0; i < clearTime; i++)
+        {
+            yield return null; //wait 20 frames
+        }
+        label.text = "";
+    }
+}
